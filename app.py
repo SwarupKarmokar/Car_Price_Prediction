274e3642281b839563eff6d29e500018c7aa1bfa
Swarup karmokar <59216779+SwarupKarmokar@users.noreply.github.com>
2020-07-15 15:57:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def predict():
 
     output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='Your Car Price: {}'.format(output))
+    return render_template('index.html', prediction_text='Your Car Price: {} Lakhs'.format(output))
 
 
 if __name__ == '__main__':
